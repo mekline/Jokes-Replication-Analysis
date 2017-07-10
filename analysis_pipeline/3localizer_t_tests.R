703aad8f0c92359578288ef708faf0313d093ec1
mekline <melissa.e.kline@gmail.com>
2017-07-10 15:00:26 -0400

    Updated t-test grouping for separate columns for ROI group and task

--- analysis_pipeline/3localizer_t_tests.R
+++ analysis_pipeline/3localizer_t_tests.R
@@ -7,112 +7,19 @@ library(dplyr)
 library(pwr)
 
 #Set wd!
-setwd("~/Dropbox/_Projects/Jokes - fMRI/Jokes-Analysis Repository/Analyses_paper/contrasts")
+setwd("/Users/mekline/Dropbox/_Projects/Jokes - fMRI/Jokes-Replication-Analysis/analysis_pipeline")
 
-#######
-# Read in all contrast values
-#######
+#Make sure allSigChange is loaded. If it's not, run 2figs_resp_jokes.R to at least line 108
+View(allSigChange)
 
-# Add in the contrast and ROI names so it's not just numbers!!!!!
-
-RHLangROI.Names = c('RPostTemp', 'RAntTemp', 'RAngG', 'RIFG',      'RMFG',     'RIFGorb');
-LangROI.Names = c('LPostTemp', 'LAntTemp', 'LAngG', 'LIFG',      'LMFG',     'LIFGorb');
-
-MDROI.Names = c('LIFGop',  'RIFGop', 'LMFG',    'RMFG',    'LMFGorb',
-                'RMFGorb', 'LPrecG', 'RPrecG',  'LInsula', 'RInsula',
-                'LSMA',    'RSMA',   'LParInf', 'RParInf', 'LParSup',
-                'RParSup', 'LACC',   'RACC');
-
-ToMROI.Names = c('DMPFC', 'LTPJ',  'MMPFC', 'PC',
-                 'RTPJ',  'VMPFC', 'RSTS');
-
-
-lang.contrasts = c('sent','non','sent-non')
-revlang.contrasts = c('sent','non','non-sent')
-md.contrasts = c()
-tom.contrasts = c('bel','pho','bel-pho')
-
-normal.contrasts = c('joke', 'lit', 'joke-lit')
-custom.contrasts = c('low','med','high','other','paramfun')
-
-
-###RESP LOCALIZER
-myResults = read.csv('RHLangfROIsrespLangLoc.csv')%>%
-  mutate(ROIName = RHLangROI.Names[ROI]) %>%
-  mutate(contrastName = lang.contrasts[Contrast])%>%
-  mutate(Group = 'RHLang-toLang')
-allSigChange = myResults
-
-myResults = read.csv('LangfROIsrespLangLoc.csv')%>%
-  mutate(ROIName = LangROI.Names[ROI]) %>%
-  mutate(contrastName = lang.contrasts[Contrast])%>%
-  mutate(Group = 'LHLang-toLang')
-allSigChange = rbind(allSigChange, myResults)
-
-##TO ADD: MD to Lang localizer measure (Non should > Sent)
-myResults = read.csv('MDfROIsrespRevLangLoc.csv')%>%
-   mutate(ROIName = MDROI.Names[ROI]) %>%
-   mutate(contrastName = revlang.contrasts[Contrast])%>%
-   mutate(Group = 'MDall-toLang')
- allSigChange = rbind(allSigChange, myResults)
-#Little extra thing here, rename MD to split by L and R hemisphere!
-allSigChange[(allSigChange$Group == 'MDall-toLang') & (allSigChange$ROI %%2 == 1),]$Group = 'MDLeft-toLang'
-allSigChange[(allSigChange$Group == 'MDall-toLang') & (allSigChange$ROI %%2 == 0),]$Group = 'MDRight-toLang'
-
-
-myResults = read.csv('NewToMfROIsrespToMLoc.csv')%>%
-  mutate(ROIName = ToMROI.Names[ROI]) %>%
-  mutate(contrastName = tom.contrasts[Contrast])%>%
-  mutate(Group = 'ToM-toToM')
-allSigChange = rbind(allSigChange, myResults)
-
-###RESP JOKES
-
-myResults = read.csv('RHLangfROIsrespNonlitJokes.csv')%>%
-  mutate(ROIName = RHLangROI.Names[ROI]) %>%
-  mutate(contrastName = normal.contrasts[Contrast])%>%
-  mutate(Group = 'RHLang')
-allSigChange = rbind(allSigChange, myResults)
-
-myResults = read.csv('LangfROIsrespNonlitJokes.csv') %>%
-  mutate(ROIName = LangROI.Names[ROI]) %>%
-  mutate(contrastName = normal.contrasts[Contrast])%>%
-  mutate(Group = 'LHLang')
-allSigChange = rbind(allSigChange, myResults)
-
-myResults = read.csv('MDfROIsrespNonlitJokes.csv') %>%
-  mutate(ROIName = MDROI.Names[ROI]) %>%
-  mutate(contrastName = normal.contrasts[Contrast]) %>%
-  mutate(Group = 'MDAll')
-allSigChange = rbind(allSigChange, myResults)
-
-#Little extra thing here, rename MD to split by L and R hemisphere!
-allSigChange[(allSigChange$Group == 'MDAll') & (allSigChange$ROI %%2 == 1),]$Group = 'MDLeft'
-allSigChange[(allSigChange$Group == 'MDAll') & (allSigChange$ROI %%2 == 0),]$Group = 'MDRight'
-
-myResults = read.csv('NewToMfROIsrespNonlitJokes.csv')%>%
-  mutate(ROIName = ToMROI.Names[ROI]) %>%
-  mutate(contrastName = normal.contrasts[Contrast]) %>%
-  mutate(Group = 'ToM')
-allSigChange = rbind(allSigChange, myResults)
-
-
-###RESP JOKES-CUSTOM with paramfun #10/07 new thing for supp. materials
-
-myResults = read.csv('NewToMfROIsrespNonlitJokesCustom_20161007.csv')%>%
-  mutate(ROIName = ToMROI.Names[ROI]) %>%
-  mutate(contrastName = custom.contrasts[Contrast])%>%
-  mutate(Group = 'ToMCustom')
-allSigChange = rbind(allSigChange, myResults)
-
-#View(allSigChange)
-
+#For the replication, commenting this out, we'll find out in a minute if any localizer-to-localizer
+#measurements are not robust enough
 #New 10/12: Localizer analysis shows that VMPFC localizer doesn't come out in this dataset, so remove it from
 #the joke-lit tests for ToM and ToM custom (but leave it for the localizer itself)
 
-allSigChange = allSigChange %>%
-  filter(!(Group == 'ToM' & ROIName =='VMPFC')) %>%
-  filter(!(Group == 'ToMCustom' & ROIName =='VMPFC'))
+#allSigChange = allSigChange %>%
+#  filter(!(Group == 'ToM' & ROIName =='VMPFC')) %>%
+#  filter(!(Group == 'ToMCustom' & ROIName =='VMPFC'))
 
 
 #######
@@ -120,19 +27,18 @@ allSigChange = allSigChange %>%
 #######
 
 allTests <- allSigChange %>%
-  group_by(Group)%>%
+  group_by(Group, task)%>%
   summarize(familySize = length(unique(ROI))) %>%
   merge(allSigChange) %>%
-  group_by(Group, ROI, Contrast, ROIName, contrastName, familySize) %>%
+  group_by(Group, task, ROI, ROIName, contrastName, familySize) %>%
   summarise(t = t.test(sigChange, mu=0,alt='greater')$statistic, 
             p = t.test(sigChange, mu=0,alt='greater')$p.value) %>%
   ungroup()%>%
-  group_by(Group, Contrast)%>%
+  group_by(Group, contrastName)%>%
   mutate(p.adj = p.adjust(p, method="fdr", n=familySize[1]))%>%
   ungroup()
 
-#View(allTests)
-setwd("~/Dropbox/_Projects/Jokes - fMRI/Jokes-Analysis Repository/Analyses_paper/reproducible analyses")
+View(allTests)
 zz = file('localizer_t_tests_all.csv', 'w')
 write.csv(allTests, zz, row.names=FALSE)
 close(zz)
@@ -148,6 +54,9 @@ allTests <- allTests %>%
   mutate(mismatch = sig != sigCor)
 
 View(filter(allTests,mismatch))
+#In the replication set, one mismatch: Nonwords over fixation in the linguistic task, LIFG orb, is 
+#significant before but not after correction. We don't care about this bc the interesting thing from
+#that task is Sentences - Nonwords. 
 
 #Convention: when all tests go one way, report them together as follows:
 reportTests <- function(ts, ps){
