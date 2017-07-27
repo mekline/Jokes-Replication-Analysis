603a74d2f7b1a831aa23b65024233efd26bb4b95
mekline <melissa.e.kline@gmail.com>
2017-07-27 15:56:43 -0400

    moved over the code for making the behavioral graphs

--- analysis_pipeline/5behavioral_localizer correlations.R
+++ analysis_pipeline/5behavioral_localizer correlations.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(dplyr)
 library(lme4)
 library(ggplot2)
+library(bootstrap)
 
 #(set your own wd first)
 setwd("/Users/mekline/Dropbox/_Projects/Jokes - fMRI/Jokes-Replication-Analysis/analysis_pipeline")
@@ -45,7 +46,7 @@ participants$SubjectNumber = 1:nrow(participants)
 participants$ID = participants$participant
 
 allSigChange <- merge(allSigChange, participants, by=c('SubjectNumber'), all_x=TRUE, all_y=TRUE)
-#(This drops subjects who didn't get included for the Jokes analyses!)
+#(This drops any subjects who didn't get included for the Jokes analyses!)
 ####
 # Ratings
 ####
@@ -91,3 +92,146 @@ ggplot(data=bb, aes(y=sigChange, x=meanResponseChange)) +
   theme_bw() +
 ggsave(filename="behav_activation.jpg", width=3, height=3)
   
+
+######################
+#Make the behavioral graphs for basic response times and ratings, (FIG 2)
+
+# Drop NA response
+
+behavdata <- filter(behavdata, !is.na(RT)) %>%
+  filter(!is.na(response))
+
+####
+# RT
+####
+#Get average RTs per category per participant
+avgRT <- behavdata %>%
+  group_by(ID, category) %>%
+  summarise(meanRT = mean(RT))
+
+#T test
+t.test(meanRT ~ category, data=avgRT)
+#Response times are not different by condition
+
+
+####
+# Ratings
+####
+
+#Get average ratings per category per participant
+behavdata$response <- as.numeric(as.character(behavdata$response))
+avgResponse <- behavdata %>%
+  group_by(ID, category) %>%
+  summarise(meanResponse = mean(response))
+
+t.test(meanResponse ~ category, data=avgResponse)
+#Responses are different by condition! The jokes are funny!
+
+
+####
+# Graphs!
+####
+
+#sterr <- function(mylist){
+#  my_se = sd(mylist)/sqrt(length(mylist)) 
+#  
+#  return(my_se)
+#}
+
+#Edit! We should be doing bootstrapped 95% confidence intervals instead! calculate them from allSigChange
+#then merge into mystats
+
+bootup <- function(mylist){
+  foo <- bootstrap(mylist, 1000, mean)
+  return(quantile(foo$thetastar, 0.975)[1])
+}
+bootdown <- function(mylist){
+  foo <- bootstrap(mylist, 1000, mean)
+  return(quantile(foo$thetastar, 0.025)[1])
+}
+
+#Make the organized data for ggplot
+avgRT <- ungroup(avgRT)
+avgResponse <- ungroup(avgResponse)
+
+#plot millisecnds
+avgRT$meanRT <- avgRT$meanRT * 1000
+#rename categories
+avgRT$categoryLabel <- ""
+avgRT[avgRT$category == "joke",]$categoryLabel <- "Jokes"
+avgRT[avgRT$category == "nonjoke",]$categoryLabel <- "Non-Jokes"
+avgResponse$categoryLabel <- ""
+avgResponse[avgResponse$category == "joke",]$categoryLabel <- "Jokes"
+avgResponse[avgResponse$category == "nonjoke",]$categoryLabel <- "Non-Jokes"
+
+toPlotRT = avgRT %>%
+  group_by(categoryLabel)%>%
+  summarise(mean = mean(meanRT))
+
+tobootUp = avgRT %>%
+  group_by(categoryLabel)%>%
+  summarise(bootup = bootup(meanRT))
+tobootDown = avgRT %>%
+  group_by(categoryLabel)%>%
+  summarise(bootdown = bootdown(meanRT))
+
+#toPlotRT = merge(toPlotRT, toStr)
+#toPlotRT$se_up <- toPlotRT$mean + toPlotRT$sterr
+#toPlotRT$se_down <- toPlotRT$mean - toPlotRT$sterr
+toPlotRT = merge(toPlotRT, tobootUp)
+toPlotRT = merge(toPlotRT, tobootDown)
+
+toPlotResp = avgResponse %>%
+  group_by(categoryLabel)%>%
+  summarise(mean = mean(meanResponse))
+
+#toStr = avgResponse %>%
+#  group_by(categoryLabel)%>%
+#  summarise(sterrRes = sterr(meanResponse))
+
+#toPlotResp = merge(toPlotResp, toStr)
+#toPlotResp$se_up <- toPlotResp$mean + toPlotResp$sterr
+#toPlotResp$se_down <- toPlotResp$mean - toPlotResp$sterr
+
+tobootUp = avgResponse %>%
+  group_by(categoryLabel)%>%
+  summarise(bootup = bootup(meanResponse))
+tobootDown = avgResponse %>%
+  group_by(categoryLabel)%>%
+  summarise(bootdown = bootdown(meanResponse))
+
+toPlotResp = merge(toPlotResp, tobootUp)
+toPlotResp = merge(toPlotResp, tobootDown)
+
+setwd(mywd)
+
+ggplot(data=toPlotRT, aes(y=mean, x=categoryLabel)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=bootdown, ymax=bootup), colour="black", width=.1, position=position_dodge(.9)) +
+  coord_cartesian(ylim=c(0,1200)) +
+  scale_y_continuous(breaks = seq(0, 2000, 200))+
+  xlab('Stimulus type') +
+  ylab('Response time (milliseconds)') +
+  scale_fill_manual(name="", values=c("gray35", "gray60")) +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(strip.background = element_blank()) +
+  # Optional, remove for RHLang and ToMCustom since we want the legend there...
+  theme(legend.position="none")  
+ggsave(filename="behavioralrt.jpg", width=3, height=3)
+
+ggplot(data=toPlotResp, aes(y=mean, x=categoryLabel)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=bootdown, ymax=bootup), colour="black", width=.1, position=position_dodge(.9)) +
+  coord_cartesian(ylim=c(1,4)) +
+  scale_y_continuous(breaks = seq(1, 4, 1))+
+  xlab('Stimulus type') +
+  ylab('Average funny-ness rating') +
+  scale_fill_manual(name="", values=c("gray35", "gray60")) +
+  theme_bw() +
+  theme(legend.key = element_blank()) +
+  theme(strip.background = element_blank()) +
+  # Optional, remove for RHLang and ToMCustom since we want the legend there...
+  theme(legend.position="none")  
+ggsave(filename="behavioral.jpg", width=3, height=3)
+
