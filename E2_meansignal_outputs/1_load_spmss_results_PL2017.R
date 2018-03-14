73e4601c1baa326b07ba68b3832663dc7bcf17f7
mekline <melissa.e.kline@gmail.com>
2018-03-14 14:23:29 -0400

    fixed cloudy jokes custom

--- E2_meansignal_outputs/1_load_spmss_results_PL2017.R
+++ E2_meansignal_outputs/1_load_spmss_results_PL2017.R
@@ -1,21 +1,20 @@
-#New! Run this after 1format_spmss_results...to get the Cloudy analysis, which
-#since it has bins of unequal length doesn't work in the old system (and PL2017
-#produces differently formatted output)
-
-#XXXSTART HERE
+#Run this file to convert our toolbox outputs to tidy csv.  It's way easier on
+#data from the PL2017 pipeline, but make sure you do the right one. 
+#(Resulting data struct stored in variable myfile in case you want to access directly)
 
 ####
 #Stuff to change!
-myResultsFolder = paste(getwd(),'/CloudyToMfROIS_resp_Jokes_20181101',sep='')
+myResultsFolder = paste(getwd(),'/CloudyToMfROIS_mentpain_resp_JokesCustom_PL2017_20180314',sep='')
 myOutputFolder = getwd()
-myFilename = 'CloudyToMfROIS_resp_Jokes_20181101.csv'
+myFilename = 'CloudyToMfROIS_mentpain_resp_JokesCustom_PL2017_20180314.csv'
 toSave = 1
 
-#(Resulting data struct stored in variable myfile, to use directory)
+nRowsToKeep = 7 #Needed for the newest version, corresponds to the number of regions/fROIs in your mask
+  
 
-####
-# Leave alone unless feeling fancy
 
+#### VERSION FOR PL2017 DATA AS FORMATTED 3/12-3/14
+#Leave the rest alone unless you're feeling fancy
 
 library(dplyr)
 library(tidyr)
@@ -24,15 +23,59 @@ library(stringr)
 setwd(myResultsFolder)
 
 #Open the weirdly formatted files and get just the table we want.
-myfile  = read.csv('spm_ss_mROI_data.csv',sep=',', skip=1, header=FALSE)
-names(myfile) = c('ROIName','Subject','Contrast','nVoxels','SignalChange')
+myfile  = read.csv('spm_ss_mROI_data.csv',sep=',', skip=1, header=TRUE)
+lastsub = ncol(myfile)
+myfile= myfile[1:nRowsToKeep,]#drop things past the individual % changes....
 
+#To add: Look at the # of ROI parcels and their sizes, declare this to be a particular
+#localizer, provide names for parcels. Also could add all that as an optional function arg.
+
+extract_val <- function(mystring, mynum){# fn to extract subject & contrast numbers
+  foo = str_split(mystring, "\\.")
+  myval = unlist(foo[[1]][mynum])
+  return(myval)
+  
+}
+
+#Make the data beautiful and longform.
+myfile[] <- lapply(myfile, as.character) #(Everything's a string, no factors)
+myfile <- myfile %>%
+  gather("Subject_and_Cont", "sigChange", Subject.1.1.:ncol(myfile)) %>%
+  rowwise() %>%
+  mutate(SubjectNumber = extract_val(Subject_and_Cont, 2)) %>%
+  mutate(Contrast = extract_val(Subject_and_Cont, 3)) %>%
+  select(-Subject_and_Cont) %>%
+  rename(ROI = ROI.)
+
+
+#Optional: print back out a nice file with a more informative name.
 if(toSave){
   setwd(myOutputFolder)
   zz <- file(myFilename, "w")
   write.csv(myfile, zz, row.names=FALSE)
   close(zz)
 }
+
+# #### Purportedly for PL2017; note that analyses 3/12-3/14 did NOT produce this new kind of output
+# # Leave alone unless feeling fancy
+# 
+# 
+# library(dplyr)
+# library(tidyr)
+# library(stringr)
+# 
+# setwd(myResultsFolder)
+# 
+# #Open the weirdly formatted files and get just the table we want.
+# myfile  = read.csv('spm_ss_mROI_data.csv',sep=',', skip=1, header=FALSE)
+# names(myfile) = c('ROIName','Subject','Contrast','nVoxels','SignalChange')
+# 
+# if(toSave){
+#   setwd(myOutputFolder)
+#   zz <- file(myFilename, "w")
+#   write.csv(myfile, zz, row.names=FALSE)
+#   close(zz)
+# }
 
 # #### Old version, pre PL2017
 # #Leave the rest alone unless you're feeling fancy
@@ -43,13 +86,13 @@ if(toSave){
 # 
 # setwd(myResultsFolder)
 # 
-# #Open the weirdly formatted files and get just the table we want. 
+# #Open the weirdly formatted files and get just the table we want.
 # myfile  = read.csv('spm_ss_mROI_data.csv',sep=',', skip=1, header=FALSE)
-# lastsub = ncol(myfile) 
+# lastsub = ncol(myfile)
 # myfile= myfile[complete.cases(myfile[,lastsub]),]#drop things past the individual % changes....
 # 
-# #To add: Look at the # of ROI parcels and their sizes, declare this to be a particular 
-# #localizer, provide names for parcels. Also could add all that as an optional function arg. 
+# #To add: Look at the # of ROI parcels and their sizes, declare this to be a particular
+# #localizer, provide names for parcels. Also could add all that as an optional function arg.
 # 
 # extract_val <- function(mystring, mynum){# fn to extract subject & contrast numbers
 #   foo = str_split(mystring, "\\.")
@@ -60,14 +103,14 @@ if(toSave){
 # 
 # #Make the data beautiful and longform.
 # myfile[] <- lapply(myfile, as.character) #(Everything's a string, no factors)
-# myfile <- myfile %>% 
+# myfile <- myfile %>%
 #   gather("Subject_and_Cont", "sigChange", Subject.1.1.:ncol(myfile)) %>%
-#   rowwise() %>% 
+#   rowwise() %>%
 #   mutate(SubjectNumber = extract_val(Subject_and_Cont, 2)) %>%
 #   mutate(Contrast = extract_val(Subject_and_Cont, 3)) %>%
 #   select(-Subject_and_Cont) %>%
 #   rename(ROI = ROI.)
-#   
+# 
 # 
 # #Optional: print back out a nice file with a more informative name.
 # if(toSave){
